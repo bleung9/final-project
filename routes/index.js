e777177894198760e605268466ca7aa50a8bd0e3
Harkamal Khabra <khabra07@gmail.com>
2019-04-27 16:51:50 +0000

    Implement routes- register and login GET route, also add nav bar partial

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Final Project' });
+});
+
+// Register page
+router.get('/register', function(req, res, next) {
+  res.send("Add register view");
+});
+
+// Login page
+router.get('/login', function(req, res, next) {
+  res.send("Add login view");
 });
 
 module.exports = router;
