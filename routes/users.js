d01749a28fd3d516b0fe0e1145e2cfbe0d0b4702
Harkamal Khabra <khabra07@gmail.com>
2019-04-27 18:14:06 +0000

    Implement normalize.css stylesheet

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.send('Add users view');
 });
 
+// User logged in profile
+router.get('/userid', function (req, res, next) {
+  res.send('User profile here');
+});
+
+// User matches
+router.get('/userid/matches', function (req, res, next) {
+  res.send('User matches here');
+});
+
+/*
+To Do:
+chat routes using talkJS
+*/
+
 module.exports = router;
