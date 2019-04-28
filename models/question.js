19294f1b9199e9f0cc7d75da64a9f7e8bd02ad30
Bosco <bleung9@gmail.com>
2019-04-28 19:30:08 +0000

    changed Question model, fixed a minor big

--- models/question.js
+++ models/question.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Question = sequelize.define('Question', {
     question: DataTypes.TEXT,
-    answers: DataTypes.ARRAY
+    answers: DataTypes.ARRAY(DataTypes.STRING)
   }, {});
   Question.associate = function(models) {
     // associations can be defined here
