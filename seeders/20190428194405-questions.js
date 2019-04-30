d83499d32c3d40319d8485be35671fcd8f578b91
Bosco <bleung9@gmail.com>
2019-04-30 15:01:25 +0000

    addedded gender to seed file for user

--- seeders/20190428194405-questions.js
+++ seeders/20190428194405-questions.js
@@ -10,7 +10,7 @@ module.exports = {
                       { question: "Do you mind living with someone of the opposite gender? (Y/N)", answers: ["y", "n"], createdAt: new Date(), updatedAt: new Date() }, 
                       { question: "Do you prefer to have the house at a hot or cold temperature? (H/C)", answers: ["h", "c"], createdAt: new Date(), updatedAt: new Date() }, 
                       { question: "On a scale of 1-10, how important is it that your space is always kept clean?", answers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], createdAt: new Date(), updatedAt: new Date() }, 
-                      { question: "On a scale of 1-10, how much do you mind that your roommate frequently has frds/SO?", answers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], createdAt: new Date(), updatedAt: new Date() }, 
+                      { question: "On a scale of 1-10, how much do you mind that your roommate frequently has frds/SO over?", answers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], createdAt: new Date(), updatedAt: new Date() }, 
                       { question: "On a scale of 1-10, how much do you like Pikachu?", answers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], createdAt: new Date(), updatedAt: new Date() }, 
                       { question: "On a scale of 1-10, how much did you like Avengers: Endgame?", answers: ["y", "n"], createdAt: new Date(), updatedAt: new Date() } ];
     return queryInterface.bulkInsert('Questions', to_insert, {});
